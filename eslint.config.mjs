339d5cad91c8d04ea97bd51a82e922257ce710d2
PTARSONEVES <ptarsoneves@gmail.com>
2024-12-13 15:29:12 -0300

    Atualização eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { rules } from "eslint-plugin-react";
 
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-console": "off",
+      "class-methods-use-this": "off"
+    }
+  }
 ];
